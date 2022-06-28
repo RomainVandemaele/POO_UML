559dae7391de599c5cec5fbdb8245fe5e901d41a
rvdemael <romain.v@live.be>
2022-06-28 11:52:18 +0200

    exo11 : new test of exceptions

--- src/main/java/be/bf/uml/Main.java
+++ src/main/java/be/bf/uml/Main.java
@@ -3,6 +3,8 @@ package be.bf.uml;
 import be.bf.uml.entities.Man;
 import be.bf.uml.entities.Person;
 import be.bf.uml.entities.Woman;
+import be.bf.uml.exception.BeardLengthNumberException;
+import be.bf.uml.exception.DateFutureException;
 import be.bf.uml.exception.NoDemandException;
 
 import java.time.LocalDate;
@@ -11,12 +13,23 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Woman w= new Woman("Cleopatre","D'Egypte", LocalDate.of(1990,10,01));
         try {
-            w.answerMarriageDemand(true);
-        }catch (NoDemandException e) {
+            w.askInMarriage(w);
+
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        Man m = new Man("Jules","Cesar",LocalDate.of(1990,6,27));
+        try {
+            m.setBeardLength(-2);
+        }catch (BeardLengthNumberException e) {
             System.out.println(e.getMessage());
         }
 
-        Man m = new Man("Jules","Cesar",LocalDate.of(1990,6,27));
+        try {
+            m.setBirthday(LocalDate.of(2023,1,1));
+        }catch (DateFutureException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
